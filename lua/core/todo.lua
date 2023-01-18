738b83cb2b261a83856e7c256630a70a999912da
Tieu Long Ho <tieu@fedora.home>
2023-01-18 09:32:48 +0100

    format indent-blankline.lua

--- lua/core/todo.lua
+++ lua/core/todo.lua
@@ -2,3 +2,4 @@
 -- FIX: https://github.com/mg979/vim-visual-multi
 -- TODO: https://github.com/stevearc/dressing.nvim for refactor
 -- TODO: configure legend.lua
+-- TODO: https://github.com/gcmt/wildfire.vim
