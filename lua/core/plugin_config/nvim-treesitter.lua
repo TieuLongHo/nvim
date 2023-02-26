69c689a3dfc750a9a575edb442ee0480c4381f91
Tieu Long Ho <tieu@tieus-macbook-pro.home>
2023-02-26 15:05:23 +0100

    Removed incompatible plugins for mac. (noice,notify) and removed lua sumneko lines

--- lua/core/plugin_config/nvim-treesitter.lua
+++ lua/core/plugin_config/nvim-treesitter.lua
@@ -3,7 +3,6 @@
 require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
 	ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "typescript", "help", "java", "javascript" },
-
 	highlight = { enable = true },
 	indent = { enable = true, disable = { "python" } },
 	ncremental_selection = {
