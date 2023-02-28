17ffc8c40d1e4195ed8f0151ce534af54d16f604
Tieu Long Ho <tieu@tieus-macbook-pro.home>
2023-02-28 12:20:15 +0100

    change lualine divider

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -2,10 +2,12 @@
 -- See `:help lualine.txt`
 require("lualine").setup({
 	options = {
-		icons_enabled = false,
+		icons_enabled = true,
 		theme = "auto",
-		component_separators = "|",
-		section_separators = "",
+		-- 		component_separators = "|",
+		-- 		section_separators = "",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 	},
 	extensions = {
 		"fugitive",
